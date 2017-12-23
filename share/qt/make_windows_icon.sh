1da02562be648a05cb9ed8d302902def89363107
PlatinumCoreDev <william@hostingzone.co.za>
2017-12-23 21:38:31 +0200

    Initial commit

--- share/qt/make_windows_icon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# create multiresolution windows icon
-ICON_DST=../../src/qt/res/icons/PlatinumCore.ico
-
-convert ../../src/qt/res/icons/PlatinumCore-16.png ../../src/qt/res/icons/PlatinumCore-32.png ../../src/qt/res/icons/PlatinumCore-48.png ${ICON_DST}
